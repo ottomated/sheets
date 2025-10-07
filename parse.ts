2c03b3a3f27c6dd6b8124c8fa3104f16c58a1682
Ottomated <otto@ottomated.net>
2025-10-06 18:04:42 -0700

    begin xlsx parser

---
+++ parse.ts
@@ -0,0 +1,13 @@
+import { parse_xlsx } from './src/lib/import.ts';
+import { Blob } from 'node:buffer';
+import { readFile } from 'node:fs/promises';
+import { JSDOM } from 'jsdom';
+
+const dom = new JSDOM();
+globalThis.DOMParser = dom.window.DOMParser;
+const file = new Blob([
+	await readFile('/home/otto/Downloads/Otto Part Time Hours.xlsx'),
+]);
+
+const doc = await parse_xlsx(file);
+console.log(doc);
---
+++ src/lib/import.ts
@@ -0,0 +1,110 @@
+import {
+	BooleanNumber,
+	LocaleType,
+	type IWorkbookData,
+	type IWorksheetData,
+} from '@univerjs/core';
+import { BlobReader, ZipReader, TextWriter } from '@zip.js/zip.js';
+
+export async function parse_xlsx(data: Blob) {
+	const reader = new ZipReader(new BlobReader(data));
+
+	const entries = Object.fromEntries(
+		(await reader.getEntries()).map((e) => [e.filename, e]),
+	);
+
+	const parser = new DOMParser();
+
+	async function read_xml_file(
+		name: string,
+		read_rels?: true,
+	): Promise<{
+		doc: Document;
+		rels: Record<string, { type: string; target: string }> | null;
+	}>;
+	async function read_xml_file(
+		name: string,
+		read_rels?: false,
+	): Promise<Document>;
+	async function read_xml_file(name: string, should_read_rels: boolean = true) {
+		const file = entries[name];
+		if (!file || file.directory) {
+			throw new Error(name + ' not found');
+		}
+		const text = await file.getData(new TextWriter());
+		const doc = parser.parseFromString(text, 'text/xml');
+		if (!should_read_rels) return doc;
+
+		// console.log(text);
+		const last_slash = name.lastIndexOf('/');
+		const rel_name =
+			`${name.substring(0, last_slash)}/_rels/${name.substring(last_slash + 1)}.rels` as const;
+		return {
+			doc: parser.parseFromString(text, 'text/xml'),
+			rels: rel_name ? await read_rels(rel_name) : null,
+		};
+	}
+	async function read_rels(name: `${string}.rels`) {
+		const doc = await read_xml_file(name, false);
+		const relationships: Record<string, { type: string; target: string }> = {};
+		doc.querySelectorAll('Relationship').forEach((rel) => {
+			relationships[rel.getAttribute('Id')!] = {
+				type: rel.getAttribute('Type')!,
+				target: rel.getAttribute('Target')!,
+			};
+		});
+		return relationships;
+	}
+
+	const root = await read_rels('_rels/.rels');
+	const workbook_path = Object.values(root).find(
+		(rel) =>
+			rel.type ===
+			'http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument',
+	)?.target;
+	if (!workbook_path) {
+		throw new Error('no officeDocument found');
+	}
+	const workbook = await read_xml_file(workbook_path);
+	if (!workbook.rels) {
+		throw new Error('expected workbook to have rels');
+	}
+
+	const sheet_meta = [...workbook.doc.querySelectorAll('sheet')].map(
+		(sheet) => ({
+			name: sheet.getAttribute('name')!,
+			id: sheet.getAttribute('sheetId')!,
+			state: sheet.getAttribute('state')!,
+			rel_id: sheet.getAttribute('r:id')!,
+		}),
+	);
+	const sheet_root = dirname(workbook_path) + '/';
+	const sheets: Record<string, IWorksheetData> = {};
+	for (const meta of sheet_meta) {
+		const rel = workbook.rels[meta.rel_id];
+		if (!rel) throw new Error('no rel found for sheet ' + meta.name);
+		const sheet = await read_xml_file(sheet_root + rel.target);
+		sheets[meta.id] = {
+			id: meta.id,
+			name: meta.name,
+			tabColor:
+				sheet.doc.querySelector('sheetPr > tabColor')?.getAttribute('rgb') ??
+				'transparent', // TODO - ARGB -> RGB
+			hidden:
+				meta.state === 'visible' ? BooleanNumber.FALSE : BooleanNumber.TRUE,
+		};
+	}
+
+	const new_sheet: Omit<IWorkbookData, 'id' | 'appVersion'> = {
+		name: 'TODO',
+		locale: LocaleType.EN_US,
+		styles: {},
+		sheetOrder: sheet_meta.map((sheet) => sheet.id),
+		sheets,
+	};
+	return new_sheet;
+}
+
+function dirname(path: string) {
+	return path.substring(0, path.lastIndexOf('/'));
+}
